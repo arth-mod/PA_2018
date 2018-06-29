33c17c6e982ce579d74ad27d2a920b984407863f
arth-mod <esilvestri12@gmail.com>
2018-06-29 20:06:00 +0200

    modified main

--- src/prova/commit/ClasseProva.java
+++ src/prova/commit/ClasseProva.java
@@ -1,7 +1,7 @@
 package prova.commit;
 
 public class ClasseProva {
- public static void main(String[] args) {
-	System.out.println("ciao");
-}
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
 }
