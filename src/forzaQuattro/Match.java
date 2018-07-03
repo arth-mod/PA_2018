0b8b25ae010402d73fad507fbd235f1450bd952c
arth-mod <esilvestri12@gmail.com>
2018-07-03 12:27:38 +0200

    stampa la griglia

--- src/forzaQuattro/Match.java
+++ src/forzaQuattro/Match.java
@@ -3,7 +3,8 @@ package forzaQuattro;
 public class Match {
 
 	public static void main(String[] args) {
-		
+		Grid griglia = new Grid();
+		Utility.printGrid(System.out, griglia);
 
 	}
 
---
+++ src/forzaQuattro/Utility.java
@@ -0,0 +1,31 @@
+package forzaQuattro;
+
+import java.io.PrintStream;
+
+
+public class Utility {
+
+	private static void printRowDelimiter( PrintStream writer , int size ) {
+		writer.print("    ");
+		for( int i=0 ; i<size ; i++ ) {
+			writer.print("+---");
+		}
+		writer.println("+");
+	}
+	
+	private static void printRow(PrintStream writer, int size) {
+		writer.print("    ");
+		for( int i=0 ; i<size ; i++ ) {
+			writer.print("| "+"  ");
+		}
+		writer.println("|");
+	}
+	
+	public static void printGrid(PrintStream writer, Grid griglia) {
+		printRowDelimiter(writer, griglia.column);
+		for(int i=0; i<griglia.row; i++) {
+			printRow(writer, griglia.column);
+			printRowDelimiter(writer, griglia.column);
+		}
+	}
+}
