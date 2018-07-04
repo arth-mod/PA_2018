ed63692e8fe31d16d640bcbf2e3bbed182331ebb
arth-mod <esilvestri12@gmail.com>
2018-07-04 14:55:02 +0200

    test funzionanti

--- test/forzaQuattro/CellTest.java
+++ test/forzaQuattro/CellTest.java
@@ -1,29 +1,26 @@
 package forzaQuattro;
+
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
-import forzaQuattro.Cell;
-import forzaQuattro.CellStatus;
-import forzaQuattro.Token;
 
 class CellTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
 	
 	@Test
 	void isEmpty() {
 		Cell cell=new Cell();
-		assertTrue(cell.getStatus()==CellStatus.EMPTY);
+		assertTrue(cell.getStatus() == CellStatus.EMPTY);
 	}
 	
 	@Test
 	void isFull() {
 		Cell cell=new Cell();
-		assertTrue(cell.getStatus()==CellStatus.FULL);
+		cell.setToken(new Token(Color.RED, 0));
+		assertTrue(cell.getStatus() == CellStatus.FULL);
 	}
 	
 
