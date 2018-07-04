3bae52a41feeee329ca5e6e4d44bc1dc8491cb77
manuel cretone <manuel.cretone@studenti.unicam.it>
2018-07-04 11:38:29 +0200

    da completare illegal, vorrei controllare se il token è dentro la griglia o fuori e un altro metodo per vedere se la colonna è piena

--- src/forzaQuattro/IllegalTokenLocation.java
+++ src/forzaQuattro/IllegalTokenLocation.java
@@ -2,5 +2,11 @@ package forzaQuattro;
 
 public class IllegalTokenLocation {
 
+	private final Token token;
+	private final TokenLocation location;
+	private final Grid grid;
+	
+	//vedere se il token è fuori dalla griglia
+	public 
 	
 }
