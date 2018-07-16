071248841b385daf042d9e615aa3500758f6f818
Joyod <Joyod@LAPTOP-FMQJRHTU>
2018-07-16 15:05:02 +0200

    aggiunto test

--- test/forzaQuattro/GridTest.java
+++ test/forzaQuattro/GridTest.java
@@ -3,33 +3,58 @@ package forzaQuattro;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.stream.Collectors;
-
+import java.util.Random;
 import org.junit.jupiter.api.Test;
 
 import core.Cell;
 import core.Color;
 import core.Grid;
 import core.Token;
+import exceptions.FullColumnException;
+import exceptions.IllegalTokenLocation;
 
 class GridTest {
+	Grid grid = Grid.getInstance();
 	
-	@Test
-	void inserimento() {
-		Grid griglia = Grid.getInstance();
-		try {
-			griglia.insert(new Token(Color.RED), 0);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		assertFalse(griglia.isFree(0, 0));
-	}
 	
 	@Test
 	void grigliaInizializzata() {
-		Grid griglia = Grid.getInstance();
-		assertTrue(griglia.getCell(3, 4).getColumn() == 4);
+		Random r = new Random();
+		int row = r.nextInt(6);
+		int col = r.nextInt(7);
+		
+		assertTrue(grid.getCell(row, col).getColumn() == col);
+		assertTrue(grid.getCell(row, col).getRow() == row);
+	}
+	
+	
+	@Test
+	void inserimento() {
+		
+		try {
+			grid.insert(new Token(Color.RED), 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertFalse(grid.isFree(0, 0));
+	}
+	
+
+	
+	@Test
+	void fullColumn(){
+		assertThrows(FullColumnException.class,
+				()-> {
+					int i =0;
+			while(true) {
+				grid.insert(new Token(Color.RED), 2);
+				i++;
+				assertTrue(i<=grid.getRowNumber());
+			}
+				}
+		);
 	}
 	
 	@Test
@@ -42,23 +67,24 @@ class GridTest {
 		grid.getCellDescendingDiagonal(cell).forEach(c->System.out.println(c.getRow()+" "+c.getColumn()));
 	}
 
-	@Test
-	void getLinesOfEmptyGrid() {
-		Grid grid = Grid.getInstance();
-//		long c = 
-			grid.getMethods()
-			.keySet()
-			.stream()
-			.map((getLineFunction) -> getLineFunction.apply(grid, new Cell(0,0)))
-			.collect(Collectors.toSet())
-			.forEach((line)->line//.forEach((cell) -> grid.isFree(cell.getRow(), cell.getColumn())));
-			
-					.stream()
-					.filter(cell -> grid.isFree(cell.getRow(), cell.getColumn()))
-			)
-					;
-//			.filter((line)-> line.size()>0)
-//			.count();
-//		assertTrue(c==0);
-	}
+//	@Test
+//	void getLinesOfEmptyGrid() {
+//		Grid grid = Grid.getInstance();
+////		long c = 
+//			grid.getMethods()
+//			.keySet()
+//			.stream()
+//			.map((getLineFunction) -> getLineFunction.apply(grid, new Cell(0,0)))
+//			.collect(Collectors.toSet())
+//			.forEach((line)->line//.forEach((cell) -> grid.isFree(cell.getRow(), cell.getColumn())));
+//			
+//					.stream()
+//					.filter(cell -> grid.isFree(cell.getRow(), cell.getColumn()))
+//			)
+//					;
+////			.filter((line)-> line.size()>0)
+////			.count();
+////		assertTrue(c==0);
+//	}
+	
 }
